fbe9c2314e9d056bf6e49f9a0752ab84398e3ce7
why-not-try-calmer <mrnycticorax@gmail.com>
2021-07-15 15:14:44 +0200

    cleaned up a bit
    added README
    
    - added autopep8 formatter as explicit dep
    
    - added context manager for safely handling requests to redis
    
    more cleanup
    
    added examples
    
    -removed vscode settings (sorry)
    
    ---
    
    ---
    
    ---
    
    ...

---
+++ README.md
@@ -0,0 +1,6 @@
+## Todo (name of author | need help? y/n)
+
+## To make a decision about (name of author)
+
+## Done (name of author)
+
--- defrag/modules/db.py
+++ defrag/modules/db.py
@@ -1,22 +1,77 @@
 import logging
 from os import environ as env
-from typing import Type
-from redis import Redis
+from typing import Callable, Optional, Union
+from redis import Redis, BlockingConnectionPool
+from redis.client import Pipeline
+from pottery import RedisDict
 
 logging.basicConfig()
 
 __MOD_NAME__ = "db"
 
 
-def connect_auth() -> Redis:
-    return Redis(host=env["REDIS_HOST"], port=env["REDIS_PORT"], password=env["REDIS_PWD"])
+""" 
+Using `BlockingConnectionPool` instead of the default
+`Redis` object to have some more control over connections and because the default connector 
+throws an exception when the pool is exhausted while 
+this one simply blocks until a thread becomes available.
+Implementing as a context manager to make it very easy to 
+customize connections in a fine-grained way (i.e. now we pass different settings
+to the context manager instances, or inherit from the class, to handle 
+different connection scenarios.) 
+"""
+
+class RedisPool:
+
+    def init_pool() -> BlockingConnectionPool:
+        return BlockingConnectionPool(
+            host=env["REDIS_HOST"],
+            port=env["REDIS_PORT"],
+            password=env["REDIS_PWD"],
+        )
+
+    pool: BlockingConnectionPool = init_pool()
+
+    def __init__(self, pipeline: bool = False) -> None:
+        connector = Redis(connection_pool=RedisPool.pool)
+        if pipeline:
+            self.connection = connector.pipeline()
+        else:
+            self.connection = connector
+
+    """
+    Pipelines allow for chaining multiple requests and and running them atomically, i.e. so that
+    the entire chain either succeeds or fails.
+    """
+
+    def __enter__(self) -> Union[Redis, Pipeline]:
+        return self.connection
+
+    def __exit__(self, *args, **kwargs) -> None:
+        return None
 
 
-def redis_hello_world() -> None:
-    r = connect_auth()
-    r.set("Hello", "World")
-    res = r.get("Hello").decode("utf-8")
-    assert res == "World"
+def test_RedisPool() -> None:
+    with RedisPool() as conn:
+        conn.flushall()
+        helloworld = RedisDict(
+            {"Hello": "World"}, redis=conn, key="helloworld")
+        redis_with_pottery = helloworld["Hello"]
+        plain_redis = conn.hget("helloworld", key=b'"Hello"').decode(
+            "utf-8").replace('"', '')
+        assert plain_redis == redis_with_pottery == "World"
+    print("... thread pool: OK")
+    with RedisPool(True) as pipeline:
+        with pipeline as p:
+            p.flushall()
+            p.set("Hello", "World")
+            p.get("Hello")
+            (_, _, res) = p.execute()
+            plain_redis = res.decode("utf-8").replace('"', '')
+            assert plain_redis == "World"
+    print("... pipeline: OK")
 
-if __name__ == "__main__":
-    redis_hello_world()
+
+print("db => main: Testing redis auth, pool and context manager:")
+test_RedisPool()
+print("db => main: Tests done")
