9ae63f7e3c5b01a730ac565d21224788f0138332
why-not-try-calmer <mrnycticorax@gmail.com>
2021-07-26 10:32:54 +0200

    ---

--- defrag/tests/test_cache.py
+++ defrag/tests/test_cache.py
@@ -14,14 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-import asyncio
-from pottery.deque import RedisDeque
-from defrag.modules.helpers.cache import cache, ServicesManager, Service, Controllers
+from defrag.modules.helpers.cache import cache
 from defrag.modules.helpers import QueryObject
-from defrag.modules.db.redis import RedisPool
 from defrag import app
 from fastapi.testclient import TestClient
-import pytest
 
 @cache
 def dummy_function(query: QueryObject):
@@ -39,20 +35,3 @@ def test_cache_decorator():
     # Do it twice so that it is cached
     response = client.get("/tests/cache")
     assert response.json() == {"result": "Nothing", "cached": True}
-
-@pytest.mark.asyncio
-async def test_cache_manager():
-    async def corou1() -> None:
-        await asyncio.sleep(2)
-        print("ok")
-    sm = ServicesManager
-    connection = RedisPool(flushOnInit=True).connection
-    pottery_primitive = RedisDeque([0], redis=connection, key="test_ok:cache")
-    service = Service("telegram", Controllers(corou1, corou1), None,
-                      None, None, None, None, None, None, None, None, pottery_primitive)
-    sm.add(service)
-    tg = sm.services.telegram
-    await tg.switch(on=True)
-    assert tg.is_enabled
-    tg.cache.extendleft([1, 2, 3])
-    assert list(tg.cache) == [3, 2, 1, 0]
