27785f334e7f329a51c5583c8db72a87555efd9b
why-not-try-calmer <mrnycticorax@gmail.com>
2021-07-27 10:51:16 +0200

    simpler interface

--- defrag/tests/test_requests_manager.py
+++ defrag/tests/test_requests_manager.py
@@ -4,5 +4,5 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_requests_manager():
-    async with Req("GET", "https://opensuse-docs-bot.herokuapp.com/stats") as resp:
+    async with Req("https://opensuse-docs-bot.herokuapp.com/stats") as resp:
         assert await resp.text()
