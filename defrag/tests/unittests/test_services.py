31663afd25c888b90d8fbde436f62272818c9770
why-not-try-calmer <mrnycticorax@gmail.com>
2021-07-31 15:11:30 +0200

    - added (successful on my machines) tests

--- defrag/tests/unittests/test_services.py
+++ defrag/tests/unittests/test_services.py
@@ -1,9 +1,19 @@
-from defrag import app
+from defrag import app, pretty_log
+from defrag.modules.reddit import register_service as register_reddit
+from defrag.modules.twitter import register_service as register_twitter
+
 from fastapi.testclient import TestClient
 
 
 def test_twitter():
-    """ FIXME Apparently it does not run with the proper context setup """
     with TestClient(app) as client:
+        register_twitter()
         response = client.get("/twitter")
+        assert response.status_code == 200
+
+
+def test_reddit():
+    with TestClient(app) as client:
+        register_reddit()
+        response = client.get("/reddit")
         assert response.status_code == 200
