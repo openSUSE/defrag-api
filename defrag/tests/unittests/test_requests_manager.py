8b8a4e0642dad8b7d4c2b8b13e6ad536b9d6dca0
why-not-try-calmer <mrnycticorax@gmail.com>
2021-08-13 22:13:13 +0200

    init

--- defrag/tests/unittests/test_requests_manager.py
+++ defrag/tests/unittests/test_requests_manager.py
@@ -1,8 +1,15 @@
+import asyncio
 from defrag.modules.helpers.requests import Req
 import pytest
 
 
+async def go(i: int):
+    async with Req("https://opensuse-docs-bot.herokuapp.com/stats") as resp:
+        await asyncio.sleep(100e-3)
+        return i
+
+
 @pytest.mark.asyncio
 async def test_requests_manager():
-    async with Req("https://opensuse-docs-bot.herokuapp.com/stats") as resp:
-        assert await resp.text()
+    results = [x for x in await asyncio.gather(*[go(x) for x in range(0, 100)])]
+    assert results == [x for x in range(0, 100)]
