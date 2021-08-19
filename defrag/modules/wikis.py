d1128e6117536ab1bc05791412cccb9b9c6de8ec
why-not-try-calmer <mrnycticorax@gmail.com>
2021-08-19 19:11:45 +0200

    rebased

---
+++ defrag/modules/wikis.py
@@ -0,0 +1,48 @@
+from pydantic.main import BaseModel
+from defrag.modules.helpers import Query, QueryResponse
+from defrag.modules.helpers.requests import Req
+from defrag import app
+from typing import List
+from dateutil import parser
+
+__MOD_NAME__ = "wikis"
+
+
+class WikiGenEntry(BaseModel):
+    page_id: int
+    title: str
+    index: int
+
+
+class WikiListEntry(BaseModel):
+    wordcount: int
+    timestamp: float
+    title: str
+    snippet: str
+    url: str
+
+
+def title_to_url(title: str, base_url="https://en.opensuse.org/") -> str:
+    return base_url + title.replace(" ", "_")
+
+
+async def search_wikis_as_list(keywords: str) -> List[WikiListEntry]:
+    async with Req(f"https://en.opensuse.org/api.php", params={"action": "query", "list": "search", "srwhat": "text", "srsearch": keywords, "srlimit": 50, "format": "json"}) as response:
+        if results_json := await response.json():
+            return [WikiListEntry(url=title_to_url(i["title"]), wordcount=i["wordcount"], timestamp=parser.isoparse(i["timestamp"]).timestamp(), title=i["title"], snippet=i["snippet"]) for i in results_json["query"]["search"]]
+        return []
+
+
+async def search_wikis_as_gen(keywords: str) -> List[WikiGenEntry]:
+    async with Req(f"https://en.opensuse.org/api.php", params={"action": "query", "generator": "search", "gsrsearch": keywords, "gsrlimit": "75", "gsrwhat": "text", "gsort": "relevance", "format": "json"}) as response:
+        if results_json := await response.json():
+            return [WikiGenEntry(title=i["title"], page_id=i["pageid"], index=i["index"])
+                    for i in results_json["query"]["pages"].values()]
+        return []
+
+
+@app.get(f"/{__MOD_NAME__}/search/")
+async def search(keywords: str) -> QueryResponse:
+    results = await search_wikis_as_list(keywords)
+    query = Query(service=__MOD_NAME__)
+    return QueryResponse(query=query, results=results, results_count=len(results))
