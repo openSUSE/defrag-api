2f303bb7f41da9f8a087bccb4ced5e00a3cb14f8
why-not-try-calmer <mrnycticorax@gmail.com>
2021-09-02 11:57:51 +0200

    bumped

--- defrag/modules/oauth2.py
+++ defrag/modules/oauth2.py
@@ -1,19 +1,3 @@
-# Defrag - centralized API for the openSUSE Infrastructure
-# Copyright (C) 2021 openSUSE contributors.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 from os import environ as env
 from defrag import app
 from datetime import datetime, timedelta
