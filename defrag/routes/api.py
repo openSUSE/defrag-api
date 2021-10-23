b8ca06609221606d52eb653414b04552237f2e85
Nycticorax <mrnycticorax@gmail.com>
2021-10-24 01:38:08 +0200

    - api + flathub

--- defrag/routes/api.py
+++ defrag/routes/api.py
@@ -1,4 +1,4 @@
-from defrag.modules.helpers import QueryResponse
+from defrag.modules.helpers import Query, QueryResponse
 from defrag.modules.helpers.cache_manager import Memo_Redis
 from defrag.modules.api import search
 
@@ -7,8 +7,10 @@ router = APIRouter()
 
 __ENDPOINT_NAME__ = "api"
 
+
 @router.get("/" + __ENDPOINT_NAME__ + "/search/{distribution}")
 @Memo_Redis.install_decorator("/" + __ENDPOINT_NAME__ + "/search/")
 async def api_search(keywords: str, distribution: str) -> QueryResponse:
+    query = Query(service=__ENDPOINT_NAME__)
     res = await search(keywords, distribution)
-    return QueryResponse(results=res, results_count=len(res))
+    return QueryResponse(query=query, results=res, results_count=len(res))
