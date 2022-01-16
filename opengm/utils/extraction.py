d2c76631ffc5c6b97ee74f4466352e347996acc0
KaratekHD <karatekhd@opensuse.org>
2022-01-16 18:56:32 +0100

    Complete extraction.py and database stuff

--- opengm/utils/extraction.py
+++ opengm/utils/extraction.py
@@ -1,7 +1,14 @@
+from pyrogram.errors import BadRequest
 from pyrogram.types import Message
-from pyrogram import Client
+
+from opengm.opengm import Opengm
 from opengm.utils.commands import get_args
-import opengm.plugins.sql.users 
+import opengm.plugins.sql.users as sql
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
 async def id_from_reply(message: Message):
     prev_message = message.reply_to_message
     if not prev_message:
@@ -16,12 +23,8 @@ async def extract_user_and_text(msg: Message):
     args = get_args(msg)
     split_text = msg.text.split(None, 1)
     print(split_text)
-    print(len(split_text))
     if len(split_text) < 2:
-        print("here")
         id, text = await id_from_reply(msg)
-        print(id)
-        print(text)
         return await id_from_reply(msg)
 
     text_to_parse = split_text[1]
@@ -48,10 +51,29 @@ async def extract_user_and_text(msg: Message):
         if len(res) >= 3:
             text = res[2]
 
+    # Extract from username
+    elif len(args) >= 1 and args[0][0] == '@':
+        username = args[0]
+        users = await sql.get_user_id_by_name(username)
+        # get text
+        text = " ".join(args[1:])
+        if not users:
+            pass
+        elif len(users) == 1:
+            user_id = users[0].user_id
+        else:
+            for user_obj in users:
+                try:
+                    userdat = await (await (Opengm.get_chat_member(msg.chat.id, user_obj.user_id)).user)
+                    if userdat.username == username:
+                        user_id = userdat.userdat.id
+
+                except BadRequest as excp:
+                    LOGGER.error(excp.message)
     # Extract from reply
     elif prev_msg:
         user_id, text = await id_from_reply(msg)
-    
+
     # Fallback
     else:
         return None, None
